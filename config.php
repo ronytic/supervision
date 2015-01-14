e1a17ffc48d13c9a7a079007ac385b181487beb7
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-01-13 20:23:43 -0400

    Config

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$title="Sistema de Administración";
+$siglas="";
+$directory="supervision";//ej:carp/ sin el "/"
+$url="http://localhost/";//ej:http:"localhost/"
+$lema2="";
+$lema='Productos de Calidad';
+$direccion="EDIFICIO CHUQUIAGO CALLLE MEXICO Nro 1551 P.B. OF 1 TEL: 2114043 - 70642896";
+$telefono="2482516";
+
+$ta4="0aXzh++RAPaeH9XPUXzAaLjYAhOByL5jWvmip5hP0CSy2pP+8wyPegfbzLZu13exmtof0B3aEtM9Wid05YtJmQ==";
+$tp="D3W1N8E0E4B4B4";
+?>
