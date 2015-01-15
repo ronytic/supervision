2ea05540f46cd96e1f87763c200d4af369c0252b
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-01-15 11:48:38 -0400

    Cabecera

--- cabecera.php
+++ cabecera.php
@@ -3,7 +3,7 @@
 
 	<div id="header">
 		<div id="logo">
-			<h1><a href="<?php echo $folder?>">Supervisión de obras G.A.M.E.A.   </a></h1>			
+			<h1><a href="<?php echo $folder?>"><img src="<?php echo $folder?>imagenes/logotrafico.jpg" height="100"> Supervisión de obras G.A.M.E.A.   </a></h1>			
 		</div>
 	</div>
 	<!--  cabesera -->
