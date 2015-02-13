67ec4b574bbaf88ea821ff5d411a4b5e6a5d04d8
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-02-12 21:20:32 -0400

    Corregido Menu y Submenu

--- class/submenu.php
+++ class/submenu.php
@@ -6,9 +6,12 @@ class submenu extends bd{
 		$this->campos=array("*");
 		switch ($codUsuario) {
 			case 1:{return $this->getRecords("superadmin=1 and idmenu=$menu and activo=1","orden");}break;
-			case 2:{return $this->getRecords("gerente=1 and idmenu=$menu and activo=1","orden");}break;
-			case 3:{return $this->getRecords("encargadoventas=1 and idmenu=$menu and activo=1","orden");}break;
-			case 4:{return $this->getRecords("tecnicos=1 and idmenu=$menu and activo=1","orden");}break;
+			case 2:{return $this->getRecords("director=1 and idmenu=$menu and activo=1","orden");}break;
+			case 3:{return $this->getRecords("jefeunidad=1 and idmenu=$menu and activo=1","orden");}break;
+			case 4:{return $this->getRecords("supervisor=1 and idmenu=$menu and activo=1","orden");}break;
+			case 5:{return $this->getRecords("gestion=1 and idmenu=$menu and activo=1","orden");}break;
+			case 6:{return $this->getRecords("ventanilla=1 and idmenu=$menu and activo=1","orden");}break;
+			case 7:{return $this->getRecords("correspondencia=1 and idmenu=$menu and activo=1","orden");}break;
 		}
 	}
 }
